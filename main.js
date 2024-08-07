2ca18f865c86a2e120e4b47dfa1737d9b3e044a1
Peter Grace <petergrace1.618@gmail.com>
2024-08-07 03:43:53 -0700

    Refactor Point.quadrantAddend() to Point.calculateTheta(). Change colors of point and angle.

--- main.js
+++ main.js
@@ -14,34 +14,35 @@ ctx.scale(3, -3);
 function Point(x, y) {
   this.x = x;
   this.y = y;
-  
-  const theta = Math.atan(this.y / this.x) + quadrantAddend(this);
+
+  const theta = calculateTheta(this);
   const thetaDeg = theta * 360 / (2 * Math.PI);
 
   this.theta = () => theta;
+  this.thetaDeg = () => thetaDeg;
 
-  this.thetaDeg = () => thetaDeg; //theta * 360 / (2 * Math.PI);
+  function calculateTheta(p) {
+    let theta = Math.atan(p.y / p.x);
 
-  // Adds a multiple of PI based on the quadrant of the point
-  // to give an angle in the range [0, 2 * PI)
-  function quadrantAddend(p) {
     // Converts the result of sign (-1, 0, 1) to 
     // (0, 1, 2) for use as index into multiplier array
     const x = Math.sign(p.x) + 1;
     const y = Math.sign(p.y) + 1;
     
+    // Adds a multiple of PI based on the quadrant of the point
+    // to give an angle in the range [0, 2 * PI)
     const multiplier = [
       [1, 2, 2],
       [1, 0, 0],
       [1, 0, 0]
     ];
-    return multiplier[y][x] * Math.PI;
+    return theta + multiplier[y][x] * Math.PI;
   }  
 }
 
 
 Point.prototype.draw = function() {
-  ctx.fillStyle = 'rgb(200 50 100)';
+  ctx.fillStyle = 'hsl(16, 60%, 50%)'; 
   ctx.beginPath();
   ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
   ctx.closePath();
@@ -55,7 +56,7 @@ Point.prototype.drawAngle = function () {
   ctx.arc(0, 0, 8, 0, this.theta())
   ctx.stroke();
   
-  ctx.strokeStyle = 'rgb(0 128 255 / 50%)';
+  ctx.strokeStyle = 'rgb(0 200 255 / 30%)'
   ctx.beginPath()
   ctx.moveTo(0, 0);
   ctx.lineTo(this.x, this.y);
@@ -91,10 +92,12 @@ function showData() {
   const theta = p.theta();
   const rad = theta.toPrecision(5);
   const deg = p.thetaDeg().toPrecision(4);
-  coordsEl.innerText = `(${canvasX}, ${canvasY})`;
+  const cx = ('' + canvasX).padStart(4, ' ');
+  const cy = ('' + canvasY).padStart(4, ' ');
+  coordsEl.innerText = `(${cx},${cy})`;
   thetaEl.innerText = '\u03b8 = ';
   thetaEl.innerText += 
-    theta ? ` ${rad} rad, ${deg}\u00b0` : ' undefined';
+  !isNaN(theta) ? ` ${rad} rad, ${deg}\u00b0` : ' undefined'; 
 }
 
 
@@ -107,7 +110,7 @@ const scaleFactor = 3
 cvs.addEventListener('mousemove', e => {
   canvasX = Math.floor(((e.x - cvs.offsetLeft) - width / 2) / scaleFactor);
   canvasY = Math.floor(((e.y - cvs.offsetTop) - height / 2) / -scaleFactor);
-  // showData();
+  
   if (e.buttons == 1) {
     draw();
   }
